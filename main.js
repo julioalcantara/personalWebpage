64f29da777b855347d220da4f24571c8b9ec1d54
julioalcantara <alcantara.juliosousa@gmail.com>
2020-10-30 19:16:16 +0000

    webpage version 1.0

---
+++ main.js
@@ -0,0 +1,18 @@
+const upArrowBtn = document.querySelector('#top-button');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    upArrowBtn.style.display = "block";
+  } else {
+    upArrowBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
